34507c02db58b7f7be3079d8cc8c8e5211578e40
jcutting <jcutting@users.noreply.github.com>
2015-10-03 17:54:03 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,5 @@ variable "cidr" { }
 variable "public_subnets" { }
 variable "private_subnets" { }
 variable "azs" { }
+variable "enable_dns_support" { default = "true" }
+variable "enable_dns_hostnames" { default = "false" }
